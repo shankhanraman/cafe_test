715d56b15b58af35054638f673a807f4664e76eb
Mayank <workformayank0@gmail.com>
2026-06-21 19:52:31 +0530

    init frontend

---
+++ infra/terraform/iam.tf
@@ -0,0 +1,33 @@
+# IAM user for GitHub Actions to push to ECR
+resource "aws_iam_user" "github_actions" {
+  name = "${var.project_name}-github-actions"
+}
+
+resource "aws_iam_access_key" "github_actions" {
+  user = aws_iam_user.github_actions.name
+}
+
+resource "aws_iam_user_policy" "ecr_push" {
+  name = "${var.project_name}-ecr-push"
+  user = aws_iam_user.github_actions.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
